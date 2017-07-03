0651622438db0405e6dbe380496c28b48410af62
Harutyun Mnatsakanyan <harmn90@gmail.com>
2017-07-03 13:45:14 +0200

    add npm modules

---
+++ src/environments/parameters.ts
@@ -0,0 +1,5 @@
+export const DECENT_NETWORK_WSPATH = 'ws://88.99.251.199:8091';
+export const DECENTGO_URL = 'http://stage-api.decentgo.com';
+export const IPFS_SERVER = 'stage.decentgo.com';
+export const IPFS_PORT = 5001;
+export const CHAIN_ID = '4777b283f8006237590c67a5001fb62e14fdfd2c0f5f5afb55e687ae8082d483';
